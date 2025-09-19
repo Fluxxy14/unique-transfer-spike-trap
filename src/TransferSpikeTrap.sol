2eb424a2f0102daa5c3b86b1c5b36db0a770ca3a
Fluxxy14 <giventoby2020@gmail.com>
2025-09-19 17:54:01 +0200

    init

--- src/TransferSpikeTrap.sol
+++ src/TransferSpikeTrap.sol
@@ -1,42 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract TransferSpikeTrap {
+    uint256 public minAmount;
+    uint256 public maxAmount;
 
-/// @title Transfer Spike Trap Token
-/// @notice ERC20 token with anti-whale transfer spike trap.
-contract TransferSpikeTrap is ERC20, Ownable {
-    uint256 public maxTxAmount; // max allowed transfer
-    mapping(address => bool) public isExempt; // addresses that bypass trap
-
-    event TrapTriggered(address indexed from, address indexed to, uint256 amount);
-
-    constructor(uint256 _supply, uint256 _maxTxAmount)
-        ERC20("TransferSpikeTrap", "TST")
-        Ownable(msg.sender)
-    {
-        _mint(msg.sender, _supply);
-        maxTxAmount = _maxTxAmount;
-        isExempt[msg.sender] = true;
+    constructor() {
+        // safe band between 0.01 ETH and 5 ETH
+        minAmount = 0.01 ether;
+        maxAmount = 5 ether;
     }
 
-    /// @notice update max transaction amount
-    function setMaxTxAmount(uint256 _newLimit) external onlyOwner {
-        maxTxAmount = _newLimit;
+    event SafeTransfer(address indexed from, address indexed to, uint256 amount);
+
+    function transfer(address payable _to, uint256 _amount) external payable {
+        require(_amount >= minAmount, " Transfer too small");
+        require(_amount <= maxAmount, " Transfer too large");
+
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Transfer failed");
+
+        emit SafeTransfer(msg.sender, _to, _amount);
     }
 
-    /// @notice exempt address from trap (e.g. DEX, owner)
-    function setExempt(address account, bool exempt) external onlyOwner {
-        isExempt[account] = exempt;
-    }
-
-    /// @dev override ERC20 hook to apply trap
-    function _update(address from, address to, uint256 value) internal override {
-        if (!isExempt[from] && !isExempt[to]) {
-            require(value <= maxTxAmount, "Trap triggered: tx too large");
-            emit TrapTriggered(from, to, value);
-        }
-        super._update(from, to, value);
-    }
+    receive() external payable {}
 }
+
---
+++ test/TransferSpikeTrap.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+contract DummyTest is Test {
+    function testPlaceholder() public {
+        assertTrue(true);
+    }
+}
+
+
